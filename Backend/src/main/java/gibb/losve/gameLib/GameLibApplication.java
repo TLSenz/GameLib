190c7f9d5bc2d530b7fdf10e52177664a0ebf2a7
Sven Zemp <svenzemp@proton.me>
2026-03-05 10:54:12 +0100

    Inital Commit

---
+++ Backend/src/main/java/gibb/losve/gameLib/GameLibApplication.java
@@ -0,0 +1,13 @@
+package gibb.losve.gameLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibApplication.class, args);
+	}
+
+}
